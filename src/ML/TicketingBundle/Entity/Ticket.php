0915c0111ec4db81626233daa210b8b44992f06e
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2017-12-19 14:20:27 +0100

    generate the Ticket Entity

---
+++ src/ML/TicketingBundle/Entity/Ticket.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace ML\TicketingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ticket
+ *
+ * @ORM\Table(name="ml_ticket")
+ * @ORM\Entity(repositoryClass="ML\TicketingBundle\Repository\TicketRepository")
+ */
+class Ticket
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name", type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="birthday", type="date")
+     */
+    private $birthday;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="visit_day", type="datetime")
+     */
+    private $visitDay;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="serial_number", type="smallint", unique=true)
+     */
+    private $serialNumber;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return Ticket
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return Ticket
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set birthday
+     *
+     * @param \DateTime $birthday
+     *
+     * @return Ticket
+     */
+    public function setBirthday($birthday)
+    {
+        $this->birthday = $birthday;
+
+        return $this;
+    }
+
+    /**
+     * Get birthday
+     *
+     * @return \DateTime
+     */
+    public function getBirthday()
+    {
+        return $this->birthday;
+    }
+
+    /**
+     * Set visitDay
+     *
+     * @param \DateTime $visitDay
+     *
+     * @return Ticket
+     */
+    public function setVisitDay($visitDay)
+    {
+        $this->visitDay = $visitDay;
+
+        return $this;
+    }
+
+    /**
+     * Get visitDay
+     *
+     * @return \DateTime
+     */
+    public function getVisitDay()
+    {
+        return $this->visitDay;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     *
+     * @return Ticket
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set serialNumber
+     *
+     * @param integer $serialNumber
+     *
+     * @return Ticket
+     */
+    public function setSerialNumber($serialNumber)
+    {
+        $this->serialNumber = $serialNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get serialNumber
+     *
+     * @return int
+     */
+    public function getSerialNumber()
+    {
+        return $this->serialNumber;
+    }
+}
+
