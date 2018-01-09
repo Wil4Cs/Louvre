c0228435efedfb65c04fa0423e33058026b58620
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2018-01-09 09:56:40 +0100

    include stripe Checkout service for payment

--- src/ML/TicketingBundle/Controller/TicketingController.php
+++ src/ML/TicketingBundle/Controller/TicketingController.php
@@ -5,6 +5,7 @@ namespace ML\TicketingBundle\Controller;
 use ML\TicketingBundle\Entity\Bill;
 use ML\TicketingBundle\Form\BillType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class TicketingController extends Controller
 {
@@ -23,10 +24,33 @@ class TicketingController extends Controller
         return $this->render('MLTicketingBundle:Ticketing:rate.html.twig');
     }
 
-    public function bookingAction()
+    public function bookingAction(Request $request)
     {
         $bill = new Bill();
         $form = $this->createForm(BillType::class, $bill);
+
+        if($request->isMethod('POST')) {
+            \Stripe\Stripe::setApiKey("sk_test_laQV9lGOvO3Up08xhDkxpr6e");
+
+            // Get the credit card details submitted by the form
+            $token = $_POST['stripeToken'];
+
+            // Create a charge: this will charge the user's card
+            try {
+                $charge = \Stripe\Charge::create(array(
+                    "amount" => 2000, // Amount in cents
+                    "currency" => "eur",
+                    "source" => $token,
+                    "description" => "Paiement Stripe - Le Musée du Louvre"
+                ));
+                $this->addFlash("success","Bravo ça marche !");
+
+            } catch(\Stripe\Error\Card $e) {
+
+                $this->addFlash("error","Snif ça marche pas :(");
+                // The card has been declined
+            }
+        }
 
         return $this->render('MLTicketingBundle:Ticketing:booking.html.twig', array(
             'form' => $form->createView()
--- src/ML/TicketingBundle/Resources/public/js/stripe.js
+++ src/ML/TicketingBundle/Resources/public/js/stripe.js
@@ -1,31 +1,38 @@
-var handler = StripeCheckout.configure({
-    key: 'pk_test_DNvpkGJnDd2qWkgbcehBjeKA',
-    image: '../../img/louvre.png',
-    locale: 'auto'
-});
+$(function () {
 
-$('#ml_ticketingbundle_bill_save').click(function(e) {
-    e.preventDefault();
-
-    var amount = $('#basket').text(),
-        amount = amount.replace(/\$/g, '').replace(/\,/g, ''),
-        amount = parseFloat(amount),
-        amount = amount * 100,
-        email = $('#ml_ticketingbundle_bill_email_first').val();
-
-    // Open Checkout with further options:
-    handler.open({
-        name: 'Musée du Louvre',
-        description: 'Paiement des billets',
-        zipCode: false,
-        email: email,
-        allowRememberMe: false,
-        currency: 'eur',
-        amount: Math.round(amount)
+    var handler = StripeCheckout.configure({
+        key: 'pk_test_DNvpkGJnDd2qWkgbcehBjeKA',
+        image: '../../img/louvre.png',
+        locale: 'auto',
+        token: function(token){
+            var $input = $('<input type=hidden name=stripeToken />').val(token.id);
+            $('form').append($input).submit();
+        }
     });
-});
 
-// Close Checkout on page navigation:
-window.addEventListener('popstate', function() {
-    handler.close();
+    $('#ml_ticketingbundle_bill_save').click(function(e) {
+
+        var amount = $('#basket').text(),
+            amount = amount.replace(/\$/g, '').replace(/\,/g, ''),
+            amount = parseFloat(amount),
+            amount = amount * 100,
+            email = $('#ml_ticketingbundle_bill_email_first').val();
+
+        // Open Checkout with further options:
+        handler.open({
+            name: 'Musée du Louvre',
+            description: 'Paiement des billets',
+            zipCode: false,
+            email: email,
+            allowRememberMe: false,
+            currency: 'eur',
+            amount: Math.round(amount)
+        });
+        e.preventDefault();
+    });
+
+    // Close Checkout on page navigation:
+    window.addEventListener('popstate', function() {
+        handler.close();
+    });
 });
