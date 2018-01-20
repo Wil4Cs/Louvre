571e3b8cdd309030d085ac796a5612629eb75be2
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2018-01-20 08:25:25 +0100

    made a mistake 01/01 instead of 01/05

---
+++ src/ML/TicketingBundle/Validator/AfterHourValidator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilfriedcottineau
+ * Date: 20/01/2018
+ * Time: 07:52
+ */
+
+namespace ML\TicketingBundle\Validator;
+
+
+class AfterHourValidator
+{
+
+}
